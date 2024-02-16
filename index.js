73a5490118b00f9d0414e3744dd136755a7b75ce
georgemiraclee <georgemiracle2019@gmail.com>
2024-02-16 18:28:55 +0700

    learn logic

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 //routes GET method
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/hello', (request, response) => {
+  response.send('Hello World!')
 })
 
+app.get('/', (req, res) => {
+  res.send('utama!')
+})
+
+app.post('/login',(req, res)=> {
+  if (req.name === "rekel"){
+    res.send('login success')
+  }
+
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
