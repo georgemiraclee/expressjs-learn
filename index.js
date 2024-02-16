661e6a6cd471d90222f9025c6c5d4e9ca8678dc4
georgemiraclee <georgemiracle2019@gmail.com>
2024-02-16 19:03:19 +0700

    intergrate postman

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 //routes GET method
-app.get('/hello', (request, response) => {
-  response.send('Hello World!')
-})
+app.get("/hello", (request, response) => {
+  console.log({urlParam: req.query/alamat})
+  response.send("Hello World!300");
+});
 
-app.get('/', (req, res) => {
-  res.send('utama!')
-})
+app.get("/", (req, res) => {
+  res.send("utama!");
+});
 
-app.post('/login',(req, res)=> {
-  if (req.name === "rekel"){
-    res.send('login success')
-  }
-
-})
+app.post("/login", (req, res) => {
+  console.log({ requestFromOutside: req.body });
+  res.send("login berhasil");
+});
+app.put("/username", (req, res) => {
+  console.log({ updateData: req.body });
+  res.send("update berhasil");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
